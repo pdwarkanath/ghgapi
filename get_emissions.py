c5eb8c682e0eaac7864d43d705f0b6dc6195924b
Dwarkanath Prabhu <dwarkanath@cmsworldwide.com>
2021-12-09 00:33:20 -0600

    hardcoded

--- get_emissions.py
+++ get_emissions.py
@@ -5,7 +5,11 @@ from psycopg2.extras import RealDictCursor
 
 conn = connect()
 
-def get_emissions(country_id, start_year, end_year, gases):
+def get_emissions(event, context):
+    country_id = 3
+    start_year = 1995
+    end_year = 2013
+    gases = ('CO2',)
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     cur.execute('SELECT country_id as id, year, gas, value FROM emissions WHERE country_id = %s AND gas IN %s AND year BETWEEN %s AND %s', [country_id, gases, start_year, end_year])
     emissions = cur.fetchall()
