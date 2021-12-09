88afe7f1fef1f8982ef34849284026a70b9e3a9d
Dwarkanath Prabhu <dwarkanath@cmsworldwide.com>
2021-12-09 00:47:05 -0600

    cursor fixed

--- get_emissions.py
+++ get_emissions.py
@@ -9,7 +9,7 @@ def get_emissions(event, context):
     start_year = 1995
     end_year = 2013
     gases = ('CO2',)
-    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute('SELECT country_id as id, year, gas, value FROM emissions WHERE country_id = %s AND gas IN %s AND year BETWEEN %s AND %s', [country_id, gases, start_year, end_year])
     emissions = cur.fetchall()
     return json.dumps(emissions)
