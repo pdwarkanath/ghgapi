4a699e4b0f795fd202babab7fdaf39df41c684a5
Dwarkanath Prabhu <dwarkanath@cmsworldwide.com>
2021-12-09 08:03:27 -0600

    api integration

--- get_emissions.py
+++ get_emissions.py
@@ -17,10 +17,10 @@ def clean_year(year):
     return
 
 def get_emissions(event, context):
-    country_id = int(event['path'][len('/country/'):])
-    start_year = int(event['queryStringParameters']['startYear'])
-    end_year = int(event['queryStringParameters']['endYear'])
-    gases = tuple(event['queryStringParameters']['gases'].split('+'))
+    country_id = int(event['params']['path']['id'])
+    start_year = int(event['params']['querystring']['startYear'])
+    end_year = int(event['params']['querystring']['endYear'])
+    gases = tuple(event['params']['querystring']['gases'].split('+'))
     cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute('SELECT country_id as id, year, gas, value FROM emissions WHERE country_id = %s AND gas IN %s AND year BETWEEN %s AND %s', [country_id, gases, start_year, end_year])
     emissions = cur.fetchall()
