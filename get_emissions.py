1006591c0ae97c603ff03dbca4a15cf1441c6e06
Dwarkanath Prabhu <dwarkanath@cmsworldwide.com>
2021-12-10 00:45:09 -0600

    success code fixed

--- get_emissions.py
+++ get_emissions.py
@@ -70,4 +70,4 @@ def get_emissions(event, context):
     cur = conn.cursor(cursor_factory=RealDictCursor)
     cur.execute('SELECT country_id as id, year, gas, value FROM emissions WHERE country_id = %s AND gas IN %s AND year BETWEEN %s AND %s', [country_id, gases, start_year, end_year])
     emissions = cur.fetchall()
-    return json.dumps({'status': 400, 'body': emissions})
+    return json.dumps({'status': 200, 'body': emissions})
